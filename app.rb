7f4d6cfb6dc4a9d00820b1a6b0a82477fbddf990
Thieu Luu <96815689+tluu5@users.noreply.github.com>
2024-01-05 21:11:57 +0000

    added models back app.rb still runs

--- app.rb
+++ app.rb
@@ -11,6 +11,23 @@ set :database_file, 'config/database.yml'
 # Enable sessions
 enable :sessions
 
+# Define the User model
+class User < ActiveRecord::Base
+  validates_presence_of :first_name, :last_name, :email, :password
+  validates_length_of :first_name, :last_name, minimum: 3
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates_length_of :password, minimum: 8
+
+  #Hash the password before saving
+  before_save :hash_password
+
+  private
+
+  def hash_password
+    self.password = BCrypt::Password.create(password)
+  end
+end
+
 get '/' do
   erb :index
 end
