291d3cefb0309b0d391efc1553d4707ebf71d599
Thieu Luu <96815689+tluu5@users.noreply.github.com>
2024-01-05 18:24:48 +0000

    moving database connection to database.yml

--- app.rb
+++ app.rb
@@ -6,19 +6,11 @@ require './models/tv_show'
 require './config/environment'
 require 'bcrypt'
 
-# Configure the database connection
-configure :development do
-  set :database, 'sqlite3:mini_project_v3.db'
-end
-
 configure :production do
   # Set your production database configuration here
   # Example:
   # set :database, 'postgres://username:password@localhost/mydatabase'
 end
-
-# Enable sessions
-enable :sessions
 
 get '/' do
   erb :index
