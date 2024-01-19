a590e7a3f59b38c50382c51f3dcf7d495e0601ad
Thieu Luu <96815689+tluu5@users.noreply.github.com>
2024-01-19 20:52:04 +0000

    commit env

--- config/environment.rb
+++ config/environment.rb
@@ -1,36 +1,17 @@
 require "./app"
-require "active_record"
 
 configure do
   # GitHub Pages and Render deployment
   set(:public_folder, "./")
 end
+# appdev support patches
+require "appdev_support"
 
-configure :development do
-  # Configure the database connection
-  set :database, 'sqlite3:mini_project_v3.db'
-  # we would also like a nicer error page in development
-  require "better_errors"
-  require "binding_of_caller"
+#set :database_file, 'config/database.yml'
 
-  # need this configure for better errors
-  use(BetterErrors::Middleware)
-  BetterErrors.application_root = __dir__
-  BetterErrors::Middleware.allow_ip!("0.0.0.0/0.0.0.0")
-
-  # appdev support patches
-  require "appdev_support"
-
-  AppdevSupport.config do |config|
-    # config.action_dispatch = true;
-    # config.active_record = true;
-    config.pryrc = :full
-  end
-  AppdevSupport.init
+AppdevSupport.config do |config|
+  #config.action_dispatch = true;
+  config.active_record = true;
+  config.pryrc = :full
 end
-
-#configure :production do
-  # Set your production database configuration here
-  # Example:
-  # set :database, 'postgres://username:password@localhost/mydatabase'
-#end
+AppdevSupport.init
