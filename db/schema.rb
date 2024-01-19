e5c381f2766012c3f30bca98f2a2e5e0438722e8
Thieu Luu <96815689+tluu5@users.noreply.github.com>
2024-01-19 21:03:58 +0000

    commit schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,15 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_01_05_164142) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tv_shows", force: :cascade do |t|
     t.string "title"
     t.string "network"
     t.string "release_date"
     t.text "description"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_tv_shows_on_user_id"
